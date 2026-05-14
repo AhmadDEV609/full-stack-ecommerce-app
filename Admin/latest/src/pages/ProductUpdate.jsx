64beda27342e8bf3f202bd8c99cf78175b94b523
Muhammad Ahmad <ahmad.dev6635@gmail.com>
2026-05-15 00:47:04 +0500

    admin frontend fix

--- Admin/latest/src/pages/ProductUpdate.jsx
+++ Admin/latest/src/pages/ProductUpdate.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import '../css/updateProduct.css'
+import '../css/updateproduct.css'
 const apiURL = import.meta.env.VITE_BACKEND_URL;
 const ProductUpdate = () => {
 
