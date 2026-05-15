78c5afb3d6af5d44af075af2bdfff81867881819
Muhammad Ahmad <ahmad.dev6635@gmail.com>
2026-05-15 23:33:37 +0500

    signup error fix

--- frontend/latest/src/pages/Signup.jsx
+++ frontend/latest/src/pages/Signup.jsx
@@ -34,50 +34,53 @@ const Signup = () => {
     };
 
     return (
-        <div className='continer'>
+        <>
             <ToastContainer />
-            <img className='image' src={image} alt="" />
+            <div className='continer'>
 
-            <div className='signupform'>
-                <h1>Signup</h1>
+                <img className='image' src={image} alt="" />
 
-                <form onSubmit={handleSubmit(onSubmit)} className='form'>
+                <div className='signupform'>
+                    <h1>Signup</h1>
 
-                    <input
-                        {...register("name", { required: "Name required" })}
-                        placeholder="Name"
-                        className='input'
-                    />
-                    {errors.name && <p>{errors.name.message}</p>}
+                    <form onSubmit={handleSubmit(onSubmit)} className='form'>
 
-                    <input
-                        {...register("email", { required: "Email required" })}
-                        placeholder="Email"
-                        className='input'
-                    />
-                    {errors.email && <p>{errors.email.message}</p>}
+                        <input
+                            {...register("name", { required: "Name required" })}
+                            placeholder="Name"
+                            className='input'
+                        />
+                        {errors.name && <p>{errors.name.message}</p>}
 
-                    <input
-                        {...register("password", { required: "Password required" })}
-                        type="password"
-                        placeholder="Password"
-                        className='input'
-                    />
-                    {errors.password && <p>{errors.password.message}</p>}
+                        <input
+                            {...register("email", { required: "Email required" })}
+                            placeholder="Email"
+                            className='input'
+                        />
+                        {errors.email && <p>{errors.email.message}</p>}
 
-                    <button className='createBtn'>Create Account</button>
+                        <input
+                            {...register("password", { required: "Password required" })}
+                            type="password"
+                            placeholder="Password"
+                            className='input'
+                        />
+                        {errors.password && <p>{errors.password.message}</p>}
 
-                    <button type="button" className='createBtn'
-                        onClick={() => window.location.href = `${apiURL}/v1/api/user/google`}
-                    >
-                        Signup with Google
-                    </button>
+                        <button className='createBtn'>Create Account</button>
 
-                    <Link style={{ color: 'black' }} to="/login">Login</Link>
+                        <button type="button" className='createBtn'
+                            onClick={() => window.location.href = `${apiURL}/v1/api/user/google`}
+                        >
+                            Signup with Google
+                        </button>
 
-                </form>
+                        <Link style={{ color: 'black' }} to="/login">Login</Link>
+
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
